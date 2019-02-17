911fed65be7fbf4882b3dfc4d0049c15c3c95d7d
Jean-vicente De Carvalho <carvalho96v2@gmail.com>
2019-02-17 14:28:56 +0200

    Got it working a bit more

--- scraper.py
+++ scraper.py
@@ -26,34 +26,43 @@ for key, value in weapon_urls.items():
 
 weapon_soups = {}
 for key, value in weapon_pages.items():
-    weapon_soups[key] = (bs(weapon_pages[key].text, 'html.parser'))
+    weapon_soups[key] = []
+    for v in value:
+        weapon_soups[key].append((bs(weapon_pages[key].text, 'html.parser')))
 
 weapon_tables = {}
 for key, value in weapon_soups.items():
-    weapon_tables[key] = weapon_soups[key].find_all('table', style = 'text-align: center; text-align:center')
+    weapon_tables[key] = []
+    for v in value:        
+        weapon_tables[key].append(weapon_soups[key].find_all('table', style = 'text-align: center; text-align:center'))
 
 category_tables = {}
 this_weapons_types = []
 for key, value in weapon_tables.items():
+    category_tables[key] = []
     this_weapons_types = weapon_types[key]
     for this_type in this_weapons_types:
-        for table in value:                                 #ToDo: find a better way to do this one day
-            if(table.tr.th.div.text == this_type):
-                category_tables[key] = value
+        for table in value:
+            for t in table:                                 #ToDo: find a better way to do this one day
+                if(t.tr.th.div.text == this_type):
+                    category_tables[key].append(table)
+
 
 weapon_paragraphs = {}
 result = []
 for key, value in category_tables.items():
+    weapon_paragraphs[key] = []
     for v in value:
-        weapon_paragraphs[key] = (v.find_all('p'))
+        for p in v:
+            weapon_paragraphs[key].append(p.find_all('p'))
 
-print(weapon_paragraphs)
 weapon_names = {}
 for key, value in weapon_paragraphs.items():
+    weapon_names[key] = []
     for v in value:
-        if v.a is not None:
-            if v.a.text is not None:
-                weapon_names[key] = v.a.text
+        for n in v:
+            if n.a is not None:
+                if n.a.text is not None:
+                    weapon_names[key].append(n.a.text)
 
-print(len(weapon_names))
 print(weapon_names)
